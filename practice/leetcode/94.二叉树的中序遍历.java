a46c1cf33cb3f0afe4c65b5c2ca1aafb8ecfabaa
cc01cc <cc01cc@foxmail.com>
2022-06-24 15:36:33 +0800

    Create 94.二叉树的中序遍历.java
    第一次使用 vsCode leetcode 插件提交

---
+++ practice/leetcode/94.二叉树的中序遍历.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=java
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        // List<Integer> list = new List<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+        in(root, list);
+        return list;
+    }
+
+    public void in(TreeNode root, List<Integer> list)
+    {
+        if (root == null)
+        {
+            return;
+        }
+        in(root.left, list);
+        list.add(root.val);
+        in(root.right, list);
+    }
+}
+// @lc code=end
+
