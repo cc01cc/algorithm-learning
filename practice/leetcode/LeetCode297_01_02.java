eb5221f6416dbf9ff18f59f449252538695578c7
cc01cc <cc01cc@foxmail.com>
2022-08-08 14:24:56 +0800

    添加注释，并优化了序列化的逻辑

--- practice/leetcode/LeetCode297_01_02.java
+++ practice/leetcode/LeetCode297_01_02.java
@@ -46,39 +46,40 @@ public class LeetCode297_01_02 {
             return "[]";
         }
 
+        // 定义一个链表用于存放二叉树
+        // 考虑到二叉树长度未知，并且后边有大量添加操作就选择了链表
         List<String> listTree = new LinkedList<>();
+        // 定义两个队列，一个存放当前的节点，一个存放下一层的节点
         Queue<TreeNode> queueTreeCur = new LinkedList<>();
         Queue<TreeNode> queueTreeNext = new LinkedList<>();
+        // 用于标记队列是否全部为 null
         Boolean queueAllNull = false;
 
         queueTreeCur.offer(root);
 
+        // 当队列全为空的时候停止循环
         while (!queueAllNull) {
             queueAllNull = true;
 
+            // 每次循环抛出当前层的节点，填充下一层的节点
+            // 如果当前层节点遍历完了，退出循环
             while (!queueTreeCur.isEmpty()) {
                 TreeNode tempNode = queueTreeCur.poll();
 
+                // 如果节点为 null 则在输出列表中添加 null
                 if (tempNode == null) {
                     listTree.add(null);
                     continue;
                 }
 
+                // 如果节点不是 null 则将值添加到输出列表 listTree 中
                 listTree.add(String.valueOf(tempNode.val));
-
-                if (tempNode.left == null) {
-                    queueTreeNext.offer(null);
-                } else {
+                
+                if (tempNode.left != null || tempNode.right != null) {
                     queueAllNull = false;
-                    queueTreeNext.offer(tempNode.left);
                 }
-
-                if (tempNode.right == null) {
-                    queueTreeNext.offer(null);
-                } else {
-                    queueAllNull = false;
-                    queueTreeNext.offer(tempNode.right);
-                }
+                queueTreeNext.offer(tempNode.left);
+                queueTreeNext.offer(tempNode.right);
 
             }
             queueTreeCur = queueTreeNext;
@@ -96,11 +97,14 @@ public class LeetCode297_01_02 {
      * @return
      */
     public TreeNode deserialize(String data) {
+        // 字符串为空；字符串所代表的二叉树为空；二叉树的根节点为空
         if (data == null || data.equals("[]") || data.startsWith("[null")) {
             return null;
         }
 
+        // 存放二叉树的每一个节点的值
         String[] strTree = data.substring(1, data.length() - 1).split(",");
+        // 存放所有二叉树节点，先进先出
         Queue<TreeNode> queueTree = new LinkedList<>();
         TreeNode root = new TreeNode(Integer.valueOf(strTree[0].trim()));
 
@@ -111,6 +115,7 @@ public class LeetCode297_01_02 {
             String strLeftNode = strTree[i].trim();
             String strRightNode = strTree[i + 1].trim();
 
+            // 将弹出节点的左孩子和右孩子若存在则放入队列中
             if (strLeftNode.equals("null")) {
                 tempNode.left = null;
             } else {
