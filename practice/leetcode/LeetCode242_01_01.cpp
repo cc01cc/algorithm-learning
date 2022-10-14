80f4091c06308a368d4057ebe88acfa613453263
cc01cc <cc01cc@foxmail.com>
2022-10-14 15:59:09 +0800

    Update LeetCode242_01_01.cpp

--- practice/leetcode/LeetCode242_01_01.cpp
+++ practice/leetcode/LeetCode242_01_01.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    bool isAnagram(string s, string t) {
+	bool isAnagram(string s, string t)
+	{
 		int alphabetArray[26] = {0};
-		for (int i = 0; i < s.size(); i++) {
-			alphabetArray[s[i] - 'a'] ++;
+		for (int i = 0; i < s.size(); i++)
+		{
+			alphabetArray[s[i] - 'a']++;
 		}
-		for (int i = 0; i < t.size(); i++) {
-			alphabetArray[t[i] - 'a'] --;
+		for (int i = 0; i < t.size(); i++)
+		{
+			alphabetArray[t[i] - 'a']--;
 		}
 
-		for (int i = 0; i < sizeof(alphabetArray)/sizeof(alphabetArray[0]); i++){
-			if (alphabetArray[i] != 0) return false;
+		for (int i = 0; i < sizeof(alphabetArray) / sizeof(alphabetArray[0]); i++)
+		{
+			if (alphabetArray[i] != 0)
+				return false;
 		}
 		return true;
-    }
+	}
 };
 
-int main() {
+int main()
+{
 	Solution solution;
 	bool res = solution.isAnagram("anagram", "nagaram");
-	cout << res <<endl;
+	cout << res << endl;
 }
-
